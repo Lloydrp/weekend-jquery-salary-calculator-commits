5afb8168bae8657494a5c11a19c2126b43d02a81
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-02 15:46:51 -0500

    collecting ee info and saving to array

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 $(onReady)
 
-function addEmployeeInfo() {
+let employeeList = [];
+
+function appendDom() {
 
 }
 
+function collectEmployeeInfo() {
+    let newEmployee = {
+        firstName: $("#inputFirstName").val(),
+        lastName: $("#inputLastName").val(),
+        employeeID: $("#inputEmployeeID").val(),
+        title: $("#inputTitle").val(),
+        annualSalary: $("#inputAnnualSalary").val()
+    };
+    return newEmployee;
+}
+
+function clearInputs() {
+    $("#inputFirstName").val("");
+    $("#inputLastName").val("");
+    $("#inputEmployeeID").val("");
+    $("#inputTitle").val("");
+    $("#inputAnnualSalary").val("");
+}
+
+function processEmployeeInfo() {
+    employeeList.push(collectEmployeeInfo());
+    clearInputs();
+}
+
 function eventHandlers() {
-    $("addEmployeeBtn").on("click", addEmployeeInfo)
+    $("#addEmployeeBtn").on("click", processEmployeeInfo)
 }
 
 function onReady() {
