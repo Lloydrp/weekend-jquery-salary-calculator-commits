64434ed9dcbcb0a0ea6d232615aa80ed247e4d4b
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-02 15:37:58 -0500

    setup id's and base HTML

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(onReady)
 
+function addEmployeeInfo() {
+
+}
+
+function eventHandlers() {
+    $("addEmployeeBtn").on("click", addEmployeeInfo)
+}
+
 function onReady() {
-    
+    eventHandlers()
 }  // end onReady
