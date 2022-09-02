6f79497dfc7cb0078bade415bedc87b16049cda3
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-02 17:33:07 -0500

    finished input validation and added documentation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(onReady)
 
 let employeeList = [];
 
+// Begin function to append employeeList to the DOM
 function appendDom() {
     $("#tableBody").empty();
     for (const employee of employeeList) {
@@ -19,21 +20,24 @@ function appendDom() {
     }
     $("#costMonthly").text(`${calculateTotalMonthly(employeeList)}`);
 
+    // Apply red styling to monthly if exceeds 20,000
     if (Number($("#costMonthly").text()) > 20000) {
         $("#costMonthly").addClass("redBackground");
     } else {
         $("#costMonthly").removeClass("redBackground");
     }
-}
+}  // end appendDom
 
+// Begin function to calculate monthly payroll cost
 function calculateTotalMonthly(array) {
     let sum = 0;
     for (const employee of array) {
         sum += Number(employee.annualSalary / 12);
     }
     return sum.toFixed(2);
-}
+}  // end calculateTotalMonthly
 
+// Begin function to collect and return employee info inputs
 function collectEmployeeInfo() {
     let newEmployee = {
         firstName: $("#inputFirstName").val(),
@@ -43,42 +47,62 @@ function collectEmployeeInfo() {
         annualSalary: $("#inputAnnualSalary").val()
     };
     return newEmployee;
-}
+}  // end collectEmployeeInfo
 
+// Begin function to clear inputs
 function clearInputs() {
     $("#inputFirstName").val("");
     $("#inputLastName").val("");
     $("#inputEmployeeID").val("");
     $("#inputTitle").val("");
     $("#inputAnnualSalary").val("");
-}
+}  // end clearInputs
 
+// Begin function to delete employee row
 function deleteEmployee(event) {
     $(event.target).closest("tr").remove();
     let eeChildID = $(event.target).closest("tr").children()[2];
     let arrayLocation = employeeList.findIndex((employee) => Number(employee.employeeID) === Number($(eeChildID).text()));
     employeeList.splice(arrayLocation, 1);
     appendDom();
-}
+}  // end deleteEmployee
 
+// Begin function to process the addition of an employee
 function processEmployeeInfo() {
     let foundID = false;
     let newEmployee = collectEmployeeInfo();
-    for (const employee of employeeList) {
-        if (employee.employeeID === newEmployee.employeeID) {
+    let valueArray = Object.values(newEmployee);
+    valueArray = valueArray.map((item) => {
+        return item.replaceAll(" ", "");
+    });
+
+    // Checking if any input values are blank
+    if (valueArray.includes("")) {
+        console.log("Please enter all Inputs");
+        return;
+    }
+
+    // Checking if the employee ID is already in use
+    for (let i = 0; i < employeeList.length; i++) {
+        const currentEmployee = employeeList[i];
+        if (currentEmployee.employeeID === newEmployee.employeeID) {
             foundID = true;
         }
     }
+
+    // If empoyee ID is NOT in use process with addition of employee
     if (foundID === false) {
         employeeList.push(collectEmployeeInfo());
         clearInputs();
         appendDom();
     } else {
+        // If employee ID is in use, clear iput and log error
         clearInputs();
         console.log("EmployeeID already in use!");
     }
-}
+}  // end processEmployeeInfo
 
+// Begin function to attach event handlers
 function eventHandlers() {
     $("#addEmployeeBtn").on("click", processEmployeeInfo)
     $("html").on("keypress", (event) => {
@@ -88,8 +112,9 @@ function eventHandlers() {
         }
     });
     $("#tableBody").on("click", ".deleteBtn", deleteEmployee)
-}
+}  // end eventHandlers
 
+// jQuery onReady
 function onReady() {
     eventHandlers()
 }  // end onReady
